997f808dd276714aa906be84eff5a1c8c3a4ddb6
Bakytzhan Gabdullin <create@MacBook-Air-Bakytzhan.local>
2025-12-07 14:30:54 +0500

    feat: Implement dotenv setup and read author from .env

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello, repisitory!")
+# main.py
+
+# Импортируем модули для работы с переменными окружения
+import os
+from dotenv import load_dotenv
+
+# 1. Загрузка переменных из .env-файла в окружение программы
+load_dotenv()
+
+# 4. & 5. Функция, которая читает переменную AUTHOR
+def print_author():
+    # os.getenv("AUTHOR") читает значение из окружения,
+    # которое было загружено из файла .env
+    author = os.getenv("AUTHOR") 
+    
+    # Печатаем результат
+    print(f"Автор проекта: {author}")
+
+# 6. Проверяем работу функции: вызываем ее
+print_author()
